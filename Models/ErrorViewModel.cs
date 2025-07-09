04e7bd3f83a772778424ae9ff6bc78b153bbf244
AZMI FADHILAH HASIBUAN <azmi22si@mahasiswa.pcr.ac.id>
2025-07-10 00:11:52 +0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProjekUASFrameworkSistemBK.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
