ba6e73153860c5eaf47ad4aacda03b2ab712dcc6
AZMI FADHILAH HASIBUAN <azmi22si@mahasiswa.pcr.ac.id>
2025-07-10 22:10:33 +0700

    add packages dan connect database - azmi

---
+++ Data/ProjekUASFrameworkSistemBKContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProjekUASFrameworkSistemBK.Models;
+
+namespace ProjekUASFrameworkSistemBK.Data
+{
+    public class ProjekUASFrameworkSistemBKContext : DbContext
+    {
+        public ProjekUASFrameworkSistemBKContext (DbContextOptions<ProjekUASFrameworkSistemBKContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ProjekUASFrameworkSistemBK.Models.UserLogin> UserLogin { get; set; } = default!;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,29 +1,35 @@
+using Microsoft.EntityFrameworkCore;
+using ProjekUASFrameworkSistemBK.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddDbContext<ProjekUASFrameworkSistemBKContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ProjekUASFrameworkSistemBKContext")));
+
+// Tambahkan layanan lain
 builder.Services.AddControllersWithViews();
+builder.Services.AddSession(); // Aktifkan session (harus sebelum UseSession)
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Middleware pipeline
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
+app.UseStaticFiles(); // Untuk file CSS, JS, gambar
+
 app.UseRouting();
 
+app.UseSession(); // Penting: letakkan sebelum Authorization jika pakai session login
 app.UseAuthorization();
 
-app.MapStaticAssets();
-
+// Routing default
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
-
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
